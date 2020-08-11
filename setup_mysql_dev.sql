b5ec3acc503240a4bce22e3ba395393582720520
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-11 09:14:55 -0500

    [Fix] Added some missing

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,7 @@
 -- Creates data base if doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Creates user if doesn't exist
-CREATE USER IF NOT EXISTS `hbnb_dev@localhost` IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost` IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant privileges to the user hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
 -- GRant SELECT privileges
