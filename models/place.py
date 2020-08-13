7340e79a89085bd62bf6117c4501ea710e8a4054
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-12 21:45:42 -0500

    Updated files for task 9

--- models/place.py
+++ models/place.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship, backref
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id", ondelete="CASCADE"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
@@ -18,3 +20,15 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship('Review', cascade="all, delete",
+                                backref=backref("place", cascade="all, delete"),
+                                passive_deletes=True)
+    else:
+        @property
+        def reviews(self):
+            """REturns a list of instances reviews"""
+            from models import storage
+            return {key: value for key, value in storage.all().items()
+                    if value.place_id == self.id}
--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 
 class User(BaseModel, Base):
@@ -13,3 +13,6 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
     places = relationship('Place', cascade='all, delete', backref='user')
+    reviews = relationship('Review', cascade='all, delete', 
+                            backref=backref("user", cascade="all, delete"),
+                            passive_deletes=True, single_parent=True)
