54a768ac7b9d02c6f562e80cbdd905194e418bfb
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-12 20:59:34 -0500

    Updated constraints in description from Place

--- models/place.py
+++ models/place.py
@@ -10,7 +10,7 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
