731a8902f226ca6a71578f001d4e7d81a8ce0a0b
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-19 09:40:30 -0500

    [Fix]

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,60 @@
 #!/usr/bin/python3
-"""Distributes an archive to the web servers"""
+"""This module contains functions that can be used to
+    generates a .tgz archive from the contents of the web_static and
+    distributes an archive to your web servers
+"""
+from fabric.api import local, env
+from fabric.operations import run, put
 from datetime import datetime
-from fabric.api import env, local, put, run
-from os.path import exists
+from os import path
 
 env.hosts = ['34.75.76.240', '35.229.102.53']
 
 
 def do_pack():
-    """"Makes a pack of directory on local"""
+    """Generates a .tgz archive from the contents of the web_static.
+    Returns:
+        return the archive path if the archive has been correctly generated.
+        Otherwise, it should return None.
+    """
+    t = datetime.now()
+    formated = "{}{}{}{}{}{}".format(t.year, t.month, t.day,
+                                     t.hour, t.minute, t.second)
+    file = "versions/web_static_{}.tgz".format(formated)
 
     local('mkdir -p versions')
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    final = 'versions/web_static_{}.tgz'.format(date)
+    local('tar -czvf {} web_static/'.format(file))
 
-    try:
-        local('tar -czvf {} web_static'.format(final))
-        return final
-    except:
-        return None
+    if path.exists(file):
+        return file
+    return None
 
 
 def do_deploy(archive_path):
-        """Distributes files to web servers"""
-        if not exists(archive_path):
-            return False
-        try:
-            put(archive_path, '/tmp/')
-            base = archive_path.split('/')[-1]
-            extent = base.split('.')[0]
-            release = '/data/web_static/releases/'
-            curren = '/data/web_static/current'
-            run('mkdir -p {}{}/'.format(release, extent))
-            run('tar -xzf /tmp/{} -C {}{}/'.format(base, release, extent))
-            run('rm /tmp/{}'.format(base))
-            run('mv{1}{0}/web_static/*{1}{0}/'.format(extent, release))
-            run('rm -rf {}{}/web_static'.format(release, extent))
-            run('rm -rf {}'.format(curren))
-            run('ln -fs {}{}/ {}'.format(release, extent, curren))
-            return True
-        except:
-            return False
+    """Distributes an archive to your web servers.
+    Args:
+        archive_path (str): tgz path.
+    Returns:
+        boolean: true if successful and false otherwise.
+    """
+
+    noext = archive_path.replace(".tgz", "").split("/")[1]
+    if not path.exists(archive_path):
+        return False
+
+    try:
+        put('{}'.format(archive_path), '/tmp/')
+        archive_path = archive_path.split("/")[1]
+
+        server_dir = "/data/web_static/releases/{}".format(noext)
+        run('mkdir -p {}'.format(server_dir))
+
+        run(' tar -xzf /tmp/{} -C {}'.format(archive_path, server_dir))
+        run('rm /tmp/{}'.format(archive_path))
+        run('mv {}/web_static/* {}/'.format(server_dir, server_dir))
+        run('rm -rf {}/web_static'.format(server_dir))
+        run('unlink /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(server_dir))
+    except Exception:
+        return False
+    return True
