ffb47e81d04ef3671bedf159f88b58c7d79e38a8
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-09-01 17:35:47 -0500

    Tasks from 4 to 6

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask
+
+hbnb = Flask(__name__)
+hbnb.url_map.strict_slashes = False
+
+
+@hbnb.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@hbnb.route('/hbnb')
+def hbnb_hbnb():
+    return 'HBNB'
+
+
+@hbnb.route('/c/<text>')
+def c_text(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@hbnb.route('/python')
+@hbnb.route('/python/<text>')
+def python_text(text='is_cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@hbnb.route('/number/<int:n>')
+def number(n):
+    return '{} is a number'.format(n)
+
+if __name__ == "__main__":
+    hbnb.run(host='0.0.0.0', port=5000)
